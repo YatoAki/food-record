1cdd27c5c02aae5453210831221c981c205df5c3
Yato <yatoakii@gmail.com>
2023-03-03 13:57:08 +0630

    Created: Store details

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,8 +1,42 @@
 const Store = require("../models/store");
+const Food = require("../models/food");
+const async = require('async');
 
-exports.store_details = (req,res) => {
-    res.send("NOt IMplemented: store Details")
-}
+exports.store_details = (req, res) => {
+    let data = [];
+    async.parallel([
+        (callback) => {
+            Food.find({store: req.params.id})
+            .sort({name: 1})
+            .then((food_lists) => {
+                data.push(food_lists);
+                callback();
+            })
+            .catch((err) => {
+                callback(err);
+            });
+        },
+        (callback) => {
+            Store.findById(req.params.id)
+            .then((store_data) => {
+                data.push(store_data);
+                callback();
+            })
+            .catch((err) => {
+                callback(err);
+            });
+        }
+    ], (err) => {
+        if (err) {
+            return res.status(500).send(err);
+        }
+        res.render("store_details", {
+            title: data[1].name,
+            store_data: data[1],
+            food_list: data[0]
+        });
+    });
+};
 
 exports.store_list = (req,res,next) => {
     
